5b32d696f51d9b3369648b53279dcc27d6a3daab
StianIsmar <41960375+StianIsmar@users.noreply.github.com>
2019-10-17 13:53:06 +0200

    Comepleted the form and tax calc

--- assignment6/tax.js
+++ assignment6/tax.js
@@ -1,5 +1,13 @@
 formChanged = () => {
     var income = document.getElementById('income').value;
-    var savings = document.getElementById('wealth'.value);
-    console.log(income);
+    var savings = document.getElementById('wealth').value;
+    console.log(income * savings);
+    console.log(income + savings);
+    var tax = calcTax(income, savings);
+    console.log('tax', tax);
+    document.getElementById('taxValue').value = tax;
+};
+
+calcTax = (income, savings) => {
+    return 0.35 * income + 0.25 * savings;
 };
